8e41a386fbcf76ceafd8075c1a6667bf5479ec12
Miguel Jimenez <m-a-jimenez@hotmail.com>
2019-01-24 11:13:30 +0200

    Added: search, list components

---
+++ src/BooksList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class BooksList extends Component {
+
+  render() {
+    return (
+
+    }
+  }
+
+  export default BooksList;
---
+++ src/SearchBooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class SearchBooks extends Component {
+
+  render() {
+    return (
+    );
+  }
+}
+
+export default SearchBooks;
