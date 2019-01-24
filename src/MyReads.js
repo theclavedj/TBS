bdadbfd1b8750366e7bb5d00f0a6b7b9510f9495
Miguel Jimenez <m-a-jimenez@hotmail.com>
2019-01-24 12:05:00 +0200

    added props to myreads + link to /search

--- src/MyReads.js
+++ src/MyReads.js
@@ -1,6 +1,7 @@
 //component MyReads will handle the mainpage UI, rendering the three shelfs and the search icon
 import React, { Component } from "react";
 import BooksList from "./BooksList";
+import { Link } from "react-router-dom";
 
 class MyReads extends Component {
   render() {
@@ -12,17 +13,27 @@ class MyReads extends Component {
         <div className="list-books-content">
           <div>
             <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2> {/*second shelf, title what im reading*/}
+              <h2 className="bookshelf-title">What I'm reading</h2>{" "}
+              {/*second shelf, title what im reading*/}
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <div className="open-search"> {/*search button, should redirect to search page*/}
-                    <button
-                      onClick={() => this.setState({ showSearchPage: true })}>
-                      Add a book
-                    </button>
-                  </div>
+                  {this.props.books //access props of the books and filter them to match currentreading component state
+                    .filter(book => book.shelf === "currentlyReading")
+                    .map(book => ( //create new array of books and display them into booklist shelf
+                      <li key={book.id}>
+                        <BooksList/>
+                      </li>
+                    ))}
                 </ol>
               </div>
+            </div>
+            <div className="open-search">
+              {/*search button, should redirect to search page*/}
+              <Link to="/search"> {/*added link component to create routing and redirect to /search once the icon is clicked*/}
+              <button onClick={() => this.setState({ showSearchPage: true })}>
+                Add a book
+              </button>
+              </Link>
             </div>
           </div>
         </div>
