a602e31b93c0aabd26880e30aaa887e772d089f4
Miguel Jimenez <m-a-jimenez@hotmail.com>
2019-01-24 12:33:56 +0200

    added option values to BooksList

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 class BooksList extends Component {
-
+  
   render() {
     return (
-
-    }
+      <div className="book">
+        <div className="book-top">
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">What I'm reading</option>
+          <option value="wantToRead">What I'll read next</option>
+          <option value="read">Already read that...</option>
+          <option value="none">Get rid of this</option>
+        </div>
+      </div>
+    );
   }
+}
 
-  export default BooksList;
+export default BooksList;
